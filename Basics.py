fa1606fe45c04884e2f1308e99331eebdd0d73a9
Rajendhiran E <rajendhiran.easu@liveramp.com>
2025-09-14 19:01:26 +0530

    basics interim commit

--- Basics.py
+++ Basics.py
@@ -1,1 +1,25 @@
-print("God knows All","Trust in God")
+from queue import Queue
+
+print("God knows All", "Trust in God")
+print("UserId:", 123, "Username: Ragavan", "Age:", 23, sep="|")
+
+uValue = "123"
+vValue = "12.52"
+sValue = 13.5614
+
+print(type(uValue))
+print(int(uValue))
+print(type(vValue))
+print(int(float(vValue)))
+print(f"{float(vValue):.1f}")
+print(float(vValue))
+print(f"{sValue:.2f}")
+print("{:.1f}".format(sValue))
+
+hasValue=True
+print(type(hasValue))
+print(int(hasValue))
+print(float(hasValue))
+
+complexInput = 3 + 10j
+print(complexInput)
